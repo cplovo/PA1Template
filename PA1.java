3d1a54bf300804a48b254997615afec938916c41
johnnweke-prof <141958413+johnnweke-prof@users.noreply.github.com>
2024-02-02 19:33:33 -0500

    Update PA1.java

--- PA1.java
+++ PA1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;  // Import the Scanner class
 public class PA1{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Let's Play UNO! ");
+        System.out.print("Let's Play BlackJack! ");
 
     
 
